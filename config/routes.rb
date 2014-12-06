2ab5cb54cba38a623a064888fece23b449c9321b
Larry Reaves <larry@yrral.net>
2014-12-06 10:32:46 -0500

    Revert "clean up unused devise views"
    This reverts commit 2a82d9362abc0359684b1f173f8c291fa1d3febc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +55,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  devise_for :voters
-  devise_for :legislators
+  devise_for :voters, controllers: { sessions: 'voters/sessions' }
+  devise_for :legislators, controllers: { sessions: 'legislators/sessions' }
 end
