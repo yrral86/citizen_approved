84c572b0a0dfa6ed8390f93f06c3aca82dca1c48
Larry Reaves <larry@yrral.net>
2014-12-06 11:03:38 -0500

    add additional fields to signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def states_helper
+    [['West Virginia', 'WV'],
+     ['Maryland', 'MD'],
+     ['California', 'CA']]
+  end
+
+  def options_helper(collection)
+    collection.map do |c|
+      [c.option_name, c.id]
+    end
+  end
 end
--- app/models/district.rb
+++ app/models/district.rb
@@ -1,2 +1,23 @@
 class District < ActiveRecord::Base
+  def self.senate_districts(state_code = nil)
+    collection = self.state_districts(state_code)
+    collection.where(senate: true)
+  end
+
+  def self.house_districts(state_code = nil)
+    collection = self.state_districts(state_code)
+    collection.where(senate: false)
+  end
+
+  def self.state_districts(state_code = nil)
+    collection = District.all
+    collection = collection.where(state_code: state_code) unless state_code.nil?
+    collection
+  end
+
+  def option_name
+    body = 'Senate'
+    body = 'House' unless self.senate
+    "#{state_code}- #{body} District #{number} (#{name})"
+  end
 end
