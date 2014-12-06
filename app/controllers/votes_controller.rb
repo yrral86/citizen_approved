487e758990b439a27a9ebb591ad565f10c707911
Larry Reaves <larry@yrral.net>
2014-12-06 16:42:09 -0500

    yes/no/delegate voting for all bills (needs restricted to only upcoming bills

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,25 @@
 class VotesController < ApplicationController
   def upcoming
+    # TODO: restrict based on time
+    bills = Bill.all
+    bill_ids = bills.map {|b| b.id}
+    votes = Vote.where(bill_id: bill_ids)
+    @voteless = []
+    @voted = []
+    bills.each do |b|
+      s = votes.select do |v|
+        v.bill_id == b.id
+      end.size
+      @voteless << b if s == 0
+      @voted << b if s == 1
+    end
   end
 
   def submit
+    v = Vote.where(bill_id: params[:bill_id], voter_id: current_voter.id).first
+    v = Vote.create(bill_id: params[:bill_id], voter_id: current_voter.id) unless v
+    v.update(choice: params[:choice]) 
+    render nothing: true
   end
 
   def compare
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,6 @@
 class Bill < ActiveRecord::Base
+  def get_vote(voter_id)
+    Vote.where(bill_id: self.id,
+               voter_id: voter_id).first
+  end
 end
