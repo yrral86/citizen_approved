d00cd21c8ceb61c5534b80d79e61471146bfd3e0
Ben Plunkert <ben@instable.net>
2014-12-07 09:24:37 -0800

    add cwd to gpg execute

--- cookbook/recipes/app.rb
+++ cookbook/recipes/app.rb
@@ -14,6 +14,7 @@ package 'libmysqlclient-dev'
 
 # set up ruby/rvm
 execute 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3' do
+  cwd '/opt/citizen_approved'
   user 'citizen_approved'
   not_if 'gpg --list-keys | grep D39DC0E3'
 end
