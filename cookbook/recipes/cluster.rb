9fc39a7306677545c3ba67817eb286e179805548
Ben Plunkert <ben@instable.net>
2014-12-06 22:19:07 -0800

    fixing hostnames in cluster.rb

--- cookbook/recipes/cluster.rb
+++ cookbook/recipes/cluster.rb
@@ -4,18 +4,18 @@ env='test'
 
 machine_batch do
   %w{app db}.each do |r|
-     machine "citizen_approved-#{env[0]}-#{r}-01" do
+     machine "citizen-#{env[0]}-#{r}-01" do
       chef_environment env
-      recipe 'citizen_approved'
+      recipe 'citizen'
       recipe 'minitest-handler'
     end
   end
 end
 
-machine "citizen_approved-#{env[0]}-db-01" do
-  recipe 'citizen_approved::mysql_master'
+machine "citizen-#{env[0]}-db-01" do
+  recipe 'citizen::mysql_master'
 end
 
-machine "citizen_approved-#{env[0]}-app-01" do
-  recipe 'citizen_approved::app'
+machine "citizen-#{env[0]}-app-01" do
+  recipe 'citizen::app'
 end
