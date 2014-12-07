3da99cb786b138e7562d185745ca0bf9971f316e
Larry Reaves <larry@yrral.net>
2014-12-07 15:35:17 -0500

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :address, :voter_id, :city, :state_code, :zip, presence: true
 end
